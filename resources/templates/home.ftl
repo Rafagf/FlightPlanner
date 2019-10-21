f9bcf2278c02e378deeca5cd61687cac40dff424
Rafagf <rafagarfer@gmail.com>
2019-10-21 15:54:26 +0100

    Html’ing Home page

---
+++ resources/templates/home.ftl
@@ -0,0 +1,12 @@
+<#import "common/bootstrap.ftl" as b>
+
+<@b.page>
+<center>
+<div class="img-with-text">
+    <img src="/static/plane.jpg" width="350" height="500" alt="Plane"/>
+    <p><i>Photo by Morre Christophe on Unsplash</i></p>
+</div>
+</center>
+</@b.page>
+
+
--- src/webapp/Home.kt
+++ src/webapp/Home.kt
@@ -1,18 +1,20 @@
 package webapp
 
 import io.ktor.application.call
+import io.ktor.freemarker.FreeMarkerContent
 import io.ktor.locations.Location
 import io.ktor.locations.get
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Route
 
-const val HOME = "home"
+const val HOME = "/"
 
 @Location(HOME)
 class Home
 
 fun Route.home() {
     get<Home> {
-        call.respondText("Home page")
+        call.respond(FreeMarkerContent("home.ftl", null))
     }
 }
