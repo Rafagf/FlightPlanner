db8739691347ae40de54235a9bd0379b2ac49712
Rafagf <rafagarfer@gmail.com>
2019-10-26 17:06:04 +0100

    Implementing sign in and sign out

--- src/webapp/SignIn.kt
+++ src/webapp/SignIn.kt
@@ -1,20 +1,61 @@
 package webapp
 
+import com.rafag.flightplanner.MIN_PASSWORD_LENGTH
+import com.rafag.flightplanner.MIN_USER_ID_LENGTH
+import com.rafag.flightplanner.model.UserSession
 import com.rafag.flightplanner.repositories.user.UserRepository
+import com.rafag.flightplanner.userNameValid
+import com.rafag.flightplanner.webapp.flights.Flights
+import com.rafag.flightplanner.webapp.redirect
 import io.ktor.application.call
 import io.ktor.freemarker.FreeMarkerContent
+import io.ktor.http.Parameters
 import io.ktor.locations.Location
 import io.ktor.locations.get
+import io.ktor.locations.post
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.Route
+import io.ktor.sessions.get
+import io.ktor.sessions.sessions
+import io.ktor.sessions.set
 
 const val SIGN_IN = "signin"
 
 @Location(SIGN_IN)
-class SignIn
+data class SignIn(val userId: String = "", val error: String = "")
 
-fun Route.signIn(userRepository: UserRepository, userHashFunction: (String) -> String) {
+fun Route.signIn(userRepository: UserRepository, hashFunction: (String) -> String) {
+
+    post<SignIn> {
+        val signInParameters = call.receive<Parameters>()
+        val userId = signInParameters["userId"] ?: return@post call.redirect(it)
+        val password = signInParameters["password"] ?: return@post call.redirect(it)
+
+        val signInError = SignIn(userId)
+
+        val signin = when {
+            userId.length < MIN_USER_ID_LENGTH -> null
+            password.length < MIN_PASSWORD_LENGTH -> null
+            !userNameValid(userId) -> null
+            else -> userRepository.getUserWithHash(userId, hashFunction(password))
+        }
+
+        if (signin == null) {
+            call.redirect(signInError.copy(error = "Invalid username or password"))
+        } else {
+            call.sessions.set(UserSession(signin.userId))
+            call.redirect(Flights())
+        }
+    }
+
     get<SignIn> {
-        call.respond(FreeMarkerContent("signin.ftl", null))
+        val user = call.sessions.get<UserSession>()?.let { userRepository.getUser(it.userId) }
+
+        if (user != null) {
+            call.redirect(Home())
+        } else {
+            call.respond(FreeMarkerContent("signin.ftl", mapOf("userId" to it.userId, "error" to it.error), ""))
+        }
     }
 }
--- src/webapp/SignOut.kt
+++ src/webapp/SignOut.kt
@@ -1,18 +1,22 @@
 package webapp
 
+import com.rafag.flightplanner.model.UserSession
 import com.rafag.flightplanner.webapp.redirect
 import io.ktor.application.call
 import io.ktor.locations.Location
 import io.ktor.locations.get
 import io.ktor.routing.Route
+import io.ktor.sessions.clear
+import io.ktor.sessions.sessions
 
-const val SIGN_OUT = "singout"
+const val SIGN_OUT = "/signout"
 
 @Location(SIGN_OUT)
 class SignOut
 
 fun Route.signOut() {
     get<SignOut> {
+        call.sessions.clear<UserSession>()
         call.redirect(SignIn())
     }
 }
